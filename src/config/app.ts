cb069c3fba3175052f36bc8b892e3c9482a2cdb2
Manthan Mallikarjun <nahtnam@gmail.com>
2024-04-14 22:26:46 -0700

    update root domain (#3)

--- src/config/app.ts
+++ src/config/app.ts
@@ -1,8 +1,9 @@
 import { isDev } from './node';
 
-export const rootDomain = 'TODO.app';
+export const rootDomain = 'TODO.com';
 export const baseDomain = (() => {
   if (isDev) return 'localhost:3000';
+  // TODO: remove the www. if you don't plan on using it
   return `www.${rootDomain}`;
 })();
 export const baseUrl = (() => {
