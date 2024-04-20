9fe5f4b5b2f23ff65ce226eedfffc51554813eea
Manthan Mallikarjun <nahtnam@gmail.com>
2024-04-20 13:43:00 -0700

    make font rendering more flexible

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,11 @@ import { twMerge } from 'tailwind-merge';
 import { Providers } from './_components/providers';
 import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
 
 // eslint-disable-next-line import/no-default-export
 export default function RootLayout({
@@ -12,8 +16,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className="h-full text-pretty" lang="en">
-      <body className={twMerge(inter.className, 'h-full')}>
+    <html lang="en" className={twMerge(inter.variable, 'h-full text-pretty')}>
+      <body className="h-full">
         <Providers>{children}</Providers>
       </body>
     </html>
