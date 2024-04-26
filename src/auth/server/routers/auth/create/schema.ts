7528d690d572b22d7f60394119f43ab2e25037a9
Manthan Mallikarjun <nahtnam@gmail.com>
2024-04-25 23:11:58 -0700

    use model schema

--- src/auth/server/routers/auth/create/schema.ts
+++ src/auth/server/routers/auth/create/schema.ts
@@ -1,6 +1,6 @@
-import { UserCreateSchema } from '@zenstackhq/runtime/zod/models';
+import { UserSchema } from '@zenstackhq/runtime/zod/models';
 import { z } from 'zod';
 
 export const authCreateInputSchema = z.object({
-  email: UserCreateSchema.shape.email.min(1),
+  email: UserSchema.shape.email.min(1),
 });
