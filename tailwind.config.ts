fdde8fb28861ebc3b0534e6d354c76c0d3a445e1
Manthan Mallikarjun <nahtnam@gmail.com>
2024-05-12 23:13:27 -0700

    admin improvements

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,7 @@ const config: Config = {
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
-    './node_modules/@premieroctet/next-admin/dist/**/*.js',
+    './node_modules/@premieroctet/next-admin/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
