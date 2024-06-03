b61c12bab724c35fe2e80e636a5c666f8401afcc
Manthan Mallikarjun <nahtnam@gmail.com>
2024-06-03 00:42:25 -0700

    0.0.16 (#22)
    * 0.0.16
    
    * update tsconfig
    
    * update homepage

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -5,22 +5,20 @@ import { BiRightArrowAlt } from "react-icons/bi";
 export default function Home() {
 	return (
 		<section className="container mt-12">
-			<div className="prose prose-xl mx-auto">
-				<center>
-					<h1>TODO</h1>
-					<p>TODO</p>
-					<form action={signInAction}>
-						<Button
-							className="bg-foreground text-background"
-							color="default"
-							endContent={<BiRightArrowAlt />}
-							type="submit"
-							size="lg"
-						>
-							Get Started
-						</Button>
-					</form>
-				</center>
+			<div className="prose prose-lg">
+				<h1>TODO</h1>
+				<p>TODO</p>
+				<form action={signInAction}>
+					<Button
+						className="bg-foreground text-background"
+						color="default"
+						endContent={<BiRightArrowAlt />}
+						type="submit"
+						size="lg"
+					>
+						Get Started
+					</Button>
+				</form>
 			</div>
 		</section>
 	);
