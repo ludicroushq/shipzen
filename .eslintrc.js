58ca5fdf58e4cb67ff1e4bbe99afefc242018a60
Manthan Mallikarjun <nahtnam@gmail.com>
2024-06-04 01:36:09 -0700

    Revert "update linting"
    This reverts commit eef62c568af8000beca2afde85a6e39231eb5ab5.

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
     },
   },
   rules: {
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-confusing-void-expression': [
       'error',
