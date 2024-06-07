607812e2bdae6306aa9c52c92e9fa45ce18d4932
Manthan Mallikarjun <nahtnam@gmail.com>
2024-06-06 17:30:28 -0700

    remove nextui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,6 @@ const config = {
   darkMode: ['class'],
   content: [
     './src/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
     './node_modules/@premieroctet/next-admin/dist/**/*.{js,ts,jsx,tsx}',
   ],
   prefix: '',
