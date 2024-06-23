e3e67b160dce5a28e79cc28dee90bb9f2f216c03
Manthan Mallikarjun <nahtnam@gmail.com>
2024-06-23 13:25:27 -0700

    0.1.5 (#29)
    * 0.1.5
    
    * remove package-lock.json
    
    * Revert "remove package-lock.json"
    
    This reverts commit ce69833382d925060335c85f73289f826f8b9cf4.
    
    * set up dotenvx
    
    * add default env
    
    * remove sentry

--- sentry.client.config.ts
+++
@@ -1,9 +0,0 @@
-import * as Sentry from '@sentry/nextjs';
-import { sentryBaseConfig } from './sentry.base.config';
-
-Sentry.init({
-  ...sentryBaseConfig,
-  tracesSampleRate: 0,
-  replaysOnErrorSampleRate: 0,
-  replaysSessionSampleRate: 0,
-});
