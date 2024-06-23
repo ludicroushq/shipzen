21409f76efc49fc269e014a0989d978dc6e7b821
Manthan Mallikarjun <nahtnam@gmail.com>
2024-06-23 13:19:17 -0700

    add default env

--- src/config/env.mjs
+++ src/config/env.mjs
@@ -5,19 +5,17 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
+    AUTH_SECRET: z.string().min(0),
     DATABASE_URL: z.string().url(),
+    DATABASE_DIRECT_URL: z.string().url(),
     IS_STANDALONE: z
       .string()
       .optional()
       .transform((s) => s === 'true'),
     SMTP_URL: z.string().url(),
   },
-  client: {
-    NEXT_PUBLIC_SENTRY_DSN: z.string().url(),
-  },
-  experimental__runtimeEnv: {
-    NEXT_PUBLIC_SENTRY_DSN: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  },
+  client: {},
+  experimental__runtimeEnv: {},
   emptyStringAsUndefined: true,
   skipValidation:
     Boolean(process.env.SKIP_ENV_VALIDATION) ||
