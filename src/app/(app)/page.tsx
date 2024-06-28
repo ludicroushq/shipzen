56299f4df4f55f6c83b98e0945172f1d90974a2e
Manthan Mallikarjun <nahtnam@gmail.com>
2024-06-28 02:04:59 -0700

    add analytics

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,3 +1,4 @@
+import { analytics } from "@/analytics";
 import { signInAction } from "@/auth/actions";
 import { ArrowRightIcon } from "lucide-react";
 
@@ -8,7 +9,13 @@ export default function Home() {
         <h1>TODO</h1>
         <p>TODO</p>
         <form action={signInAction}>
-          <button className="btn btn-neutral" type="submit">
+          <button
+            className="btn btn-neutral"
+            type="submit"
+            onClick={() => {
+              analytics.track("getStarted", { path: "/api/auth" });
+            }}
+          >
             Get Started <ArrowRightIcon className="ml-2 h-4 w-4" />
           </button>
         </form>
