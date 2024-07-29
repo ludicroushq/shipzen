7a9b14110ce851b23e9995fc624f25f315df6632
Manthan Mallikarjun <nahtnam@gmail.com>
2024-07-29 01:19:53 -0700

    more xo changes

--- xo.config.js
+++ xo.config.js
@@ -1,7 +1,11 @@
-export default {
+const config = {
 	extends: ['plugin:@next/next/core-web-vitals', 'xo-nextjs'],
+	ignore: 'next-env.d.ts',
 	prettier: true,
 	rules: {
 		'import/extensions': 'off',
+		'unicorn/prevent-abbreviations': 'off',
 	},
 };
+
+export default config;
