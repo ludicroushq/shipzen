0054cea56558619d62f4732c1706de42498169e8
Manthan Mallikarjun <nahtnam@gmail.com>
2024-07-30 21:00:00 -0700

    add prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,10 @@
 const config = {
+	bracketSpacing: false,
 	plugins: ['prettier-plugin-tailwindcss'],
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
 };
 
 export default config;
